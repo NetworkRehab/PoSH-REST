b23ab98cac0569a13383eb8a3c8122160a13a9f5
Nick Donathan <31076657+ndonathan@users.noreply.github.com>
2023-03-14 22:44:54 -0500

    Added tnc and host rest calls

--- Create-RESTServer.ps1
+++ Create-RESTServer.ps1
@@ -1,10 +1,12 @@
-# Create a listener on port 8000
+# Adapted from https://hkeylocalmachine.com/?p=518
+
+# Create a listener on port 8999
 $listener = New-Object System.Net.HttpListener
-$listener.Prefixes.Add('http://+:8000/') 
+$listener.Prefixes.Add('http://+:8999/') 
 $listener.Start()
-'Listening ...'
+'Listening for requests...'
  
-# Run until you send a GET request to /end
+# Setup Loop to listen for requests and process them
 while ($true) {
     $context = $listener.GetContext() 
  
@@ -20,24 +22,35 @@ while ($true) {
     } else {
  
         # Split request URL to get command and options
-        $requestvars = ([String]$request.Url).split("/");        
+        $requestvars = ([String]$request.Url).split("/")
  
-        # If a request is sent to http:// :8000/wmi
-        if ($requestvars[3] -eq "wmi") {
-           
-            # Get the class name and server name from the URL and run get-WMIObject
-            $result = get-WMIObject $requestvars[4] -computer $requestvars[5];
+        # If a request is sent to http:// :8000/tnc
+        Switch ($requestvars[3])
+        {
+            {$_ -match 'tnc'} 
+                {
+                    # Get the class name and server name from the URL and run get-WMIObject
+                    $result = test-connection -IPv4 -targetname $requestvars[4] -TcpPort $requestvars[5]
  
-            # Convert the returned data to JSON and set the HTTP content type to JSON
-            $message = $result | ConvertTo-Json; 
-            $response.ContentType = 'application/json';
- 
-       } else {
- 
-            # If no matching subdirectory/route is found generate a 404 message
-            $message = "This is not the page you're looking for.";
-            $response.ContentType = 'text/html' ;
-       }
+                    # Convert the returned data to JSON and set the HTTP content type to JSON
+                    $message = $result | ConvertTo-Json
+                    $response.ContentType = 'application/json'
+                }
+            {$_ -match 'host'}
+                {
+                    $result = hostname
+
+                    # Convert the returned data to JSON and set the HTTP content type to JSON
+                    $message = $result | ConvertTo-Json
+                    $response.ContentType = 'application/json'
+                }
+            default
+                {
+                    # If no matching subdirectory/route is found generate a 404 message
+                    $message = "This is not the page you're looking for."
+                    $response.ContentType = 'text/html'
+                }
+        }
  
        # Convert the data to UTF8 bytes
        [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
